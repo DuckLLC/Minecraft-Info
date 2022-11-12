2ba711424bbfa25eed968fc9254e8d1fa1fa7b7e
Brady Delgadillo <delgadillokids@outlook.com>
2022-11-11 20:23:40 -0500

    Add project files.

---
+++ Minecraft Info/MainPage.xaml.vb
@@ -0,0 +1,20 @@
+﻿' The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+''' <summary>
+''' An empty page that can be used on its own or navigated to within a Frame.
+''' </summary>
+Public NotInheritable Class MainPage
+    Inherits Page
+
+    Private Const V As Boolean = True
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Verions.Visibility = V
+        Swords.Visibility = Visibility.Collapsed
+    End Sub
+
+    Private Sub Button_Click_1(sender As Object, e As RoutedEventArgs)
+        Verions.Visibility = Visibility.Collapsed
+        Swords.Visibility = V
+    End Sub
+End Class
